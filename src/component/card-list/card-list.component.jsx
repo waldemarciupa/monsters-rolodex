da810302acc593dbf5cc10aece3f865ea3eef9c5
waldemarciupa <waldemarciupa90@gmail.com>
2020-03-09 19:59:21 +0100

    adding files for github pages

---
+++ src/component/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './card-list.style.css';
+import { Card } from '../card/card-component';
+
+const CardList = ({ monsters }) => {
+    return (
+        <div className='card-list'>
+            {
+                monsters.map((monster) => {
+                    return (
+                        <Card
+                            key={monster.id}
+                            monster={monster}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CardList;
