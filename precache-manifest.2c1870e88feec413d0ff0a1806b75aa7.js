edf5ae16a24c1696933b8bd3e6d9bfef9b1e6684
waldemarciupa <waldemarciupa90@gmail.com>
2020-03-17 13:04:25 +0100

    Updates

---
+++ precache-manifest.2c1870e88feec413d0ff0a1806b75aa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fbfb625ac7b15218987e993bd86da4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f857fcbec993e2461c81",
+    "url": "/monsters-rolodex/static/css/main.697323e5.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f857fcbec993e2461c81",
+    "url": "/monsters-rolodex/static/js/main.3d39f4b2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
